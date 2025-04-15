014a95e0e00a116eb041a168f75c53aa44813136
Conrad Thomson <conrad.thom14@gmail.com>
2025-04-15 12:51:33 +0100

    Update deployment configuration and dependencies
    - Added mobile app configuration to `ecosystem.config.js` for serving the rathburn-mobile application.
    - Updated `.gitignore` to include the new deployment zip file for the web app.
    - Added `@supabase/supabase-js` dependency to `pnpm-lock.yaml` for enhanced functionality in the application.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,5 +15,16 @@ module.exports = {
       },
       env_file: "./apps/web/.next/standalone/apps/web/.env",
     },
+    {
+      name: "rathburn-mobile",
+      script: "npx",
+      args: "serve -s ./apps/mobile/dist -l 8080",
+      cwd: "/home/ec2-user/rathburn-monorepo",
+      instances: "1",
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
   ],
 };
