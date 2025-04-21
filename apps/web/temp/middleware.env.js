d7eb84e53a3d547a66085e5ad6cfc16e58e7667b
Conrad Thomson <conrad.thom14@gmail.com>
2025-04-21 21:20:31 +0100

    complete new feature branch

--- apps/web/temp/middleware.env.js
+++
@@ -1,4 +0,0 @@
-// Export environment variables for middleware
-export const NEXT_PUBLIC_SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-export const NEXT_PUBLIC_SUPABASE_ANON_KEY =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
